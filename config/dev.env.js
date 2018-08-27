c02c0217bf560662502eff9dc0231a6019ad4fa5
Caoanlong <caoanlong@gmail.com>
2018-08-27 10:40:10 +0800

    gg

--- config/dev.env.js
+++ config/dev.env.js
@@ -8,7 +8,4 @@ module.exports = merge(prodEnv, {
 	IMG_API: '"https://api.file.szjkss.com"',
 	IMG_URL: '"https://res.file.szjkss.com"',
 	BASE_API: '"http://localhost:3002/appH5"',
-	// BASE_API: '"https://api.app.angel.jyw91.com/appH5"',
-	// IMG_API: '"http://api.pc.angel.jyw91.com"',
-	// IMG_URL: '"http://image.angel.jyw91.com"'
 })
